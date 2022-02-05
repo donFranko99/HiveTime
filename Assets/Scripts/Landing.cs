e2032bbc33987261f5af64480f620e9aeaf0b7dd
donFranko99 <franciszek.rybski@microsoft.wsei.edu.pl>
2022-02-06 00:54:55 +0100

    added resources management, landing pad with scripts
    dodalem mala platforme na duzym drzewie
    jest na niej skrypt, ktory sprawdza ile mamy pylku i staminy
    przywraca stamine po wleceniu w collider
    i jest gotowy if w srodku ktory sprawdza ilosc posiadanego pylku
    jak bedzie 20 lub wiecej to odejmuje 20 i bdzie popowal komunikat ze dziena byczq za granie w nasza giere
    jak bdzie mnie to zostawi i popnie komunikat, ze bez 20 pylku nie wpuszcza cie do ula
    do tego dodalem taki pusty obiekt ze skryptem o nazwie resources, podpiety jest pod pszczole
    przechowuje tam stamine, pylek i inne takie
    dostarcza tez metody do robienia rzeczy z tymi zmiennymi w srodku

---
+++ Assets/Scripts/Landing.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Landing : MonoBehaviour
+{
+    public BoxCollider collider;
+
+    [SerializeField] public Resources beeResources;
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnCollisionEnter()
+    {
+        if (beeResources.haveEnoughPollen(20))
+        {
+            //pop message - thank you for trying our games' pre alpha demo
+            //quit game after pressing escape
+        }
+        else
+        {
+            beeResources.resetStaminaPoints();
+        }
+    }
+}
