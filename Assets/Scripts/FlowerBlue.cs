ad1859b3d0dcba7aed728b71199925e8920acee3
donFranko99 <franciszek.rybski@microsoft.wsei.edu.pl>
2022-02-06 19:50:20 +0100

    Some flower scripts, improved steering

---
+++ Assets/Scripts/FlowerBlue.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerBlue : MonoBehaviour
+{
+    public int value;
+    public int staminaRestored;
+
+    public void Collect() { }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/FlowerRed.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerRed : MonoBehaviour
+{
+    public int value;
+    public int staminaRestored;
+
+    public void Collect()
+    {
+        //add pollen
+        //
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
